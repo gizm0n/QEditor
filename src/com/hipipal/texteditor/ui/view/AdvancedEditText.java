0dff7850982d195cfafbffc860f0fb01a03fb152
River <river@qpython.org>
2014-01-21 12:04:00 +0800

    update color

--- src/com/hipipal/texteditor/ui/view/AdvancedEditText.java
+++ src/com/hipipal/texteditor/ui/view/AdvancedEditText.java
@@ -415,7 +415,7 @@ public class AdvancedEditText extends EditText implements Constants,
 	private static final int COLOR_KEYWORD = 0xff399ed7;
 	private static final int COLOR_BUILTIN = 0xffd79e39;
 	private static final int COLOR_COMMENT = 0xff808080;
-	private static final int COLOR_QUOTE = 0xffd79e39;
+	private static final int COLOR_QUOTE = 0xff2861c6;
 
 	private static final Pattern line = Pattern.compile(
 		".*\\n" );
